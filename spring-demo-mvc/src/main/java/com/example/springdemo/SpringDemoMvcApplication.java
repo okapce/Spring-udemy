d42fac0ca5776d32ead2fddb2be276c88e8bcf40
NicoMeyer <nmeyer@bbr.cl>
2021-05-24 18:03:10 -0400

    spring mvc init + config files

---
+++ spring-demo-mvc/src/main/java/com/example/springdemo/SpringDemoMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoMvcApplication.class, args);
+	}
+
+}
