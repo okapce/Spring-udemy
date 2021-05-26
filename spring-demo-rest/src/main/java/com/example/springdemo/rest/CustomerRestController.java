f34d49af1cd4085e9c6168dec43e6f7b9a7a51b0
NicoMeyer <nmeyer@bbr.cl>
2021-05-26 17:26:38 -0400

    Rest-crm complete

--- spring-demo-rest/src/main/java/com/example/springdemo/rest/CustomerRestController.java
+++ spring-demo-rest/src/main/java/com/example/springdemo/rest/CustomerRestController.java
@@ -3,8 +3,12 @@ package com.example.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,5 +40,35 @@ public class CustomerRestController {
 		}
 		
 		return customer;
+	}
+	
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer customer) {
+		customer.setId(0); //if 0 (null), dao will insert
+		
+		customerService.saveCustomer(customer);
+		
+		return customer;
+	}
+	
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer customer) {
+		
+		customerService.saveCustomer(customer);
+		
+		return customer;
+	}
+	
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
+		Customer tempCustomer = customerService.getCustomer(customerId);
+		
+		if(tempCustomer == null) {
+			throw new CustomerNotFoundException("customer id '"+customerId+"' not found");
+		}
+		
+		customerService.deleteCustomer(customerId);
+		
+		return "Deleted customer"+customerId +" is now deleted";
 	}
 }
