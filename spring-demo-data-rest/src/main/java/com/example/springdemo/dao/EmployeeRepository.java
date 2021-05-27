700ea41d39475e70dca8cb4fcd55a0b7a7a030d4
NicoMeyer <nmeyer@bbr.cl>
2021-05-27 18:56:56 -0400

    data rest - ok

---
+++ spring-demo-data-rest/src/main/java/com/example/springdemo/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.springdemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.springdemo.entity.Employee;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
